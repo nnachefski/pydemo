4407fbdb174ac6d697beb2911b465a8ae6355596
Nick Nachefski <nnachefski@gmail.com>
2020-02-27 13:09:46 -0600

    Update setup_tekton.sh

--- tekton/setup_tekton.sh
+++ tekton/setup_tekton.sh
@@ -1,6 +1,10 @@
-oc new-project tekton-test
-oc create serviceaccount pipeline
-oc adm policy add-scc-to-user privileged -z pipeline
-oc adm policy add-role-to-user edit -z pipeline 
-oc create -f pydemo-task.yaml
-oc create -f pydemo-taskrun.yaml
+source $BASE/install/env.conf
+
+$OC_CLIENT new-project custom-images
+
+$OC_CLIENT apply -f buildah-task.yaml
+$OC_CLIENT apply -f ocp4kit-git-pr.yaml
+$OC_CLIENT apply -f s2i-base-git-pr.yaml
+$OC_CLIENT apply -f ubi8-custom-dest-image-pr.yaml
+$OC_CLIENT apply -f s2i-custom-core-dest-image-pr.yaml
+$OC_CLIENT apply -f custom-s2i-chain-pl.yaml
